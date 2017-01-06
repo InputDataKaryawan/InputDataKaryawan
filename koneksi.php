e76589d1d14583413f6104563730d55eddddc1c1
Reksi Maulana <reksimaulana1996@gmail.com>
2017-01-06 09:36:48 +0700

    hapus

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";	
-$user = "root";	
-$pass = "";	
-$name = "db_butik";
-
-$koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
-mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
-?>
